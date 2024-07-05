71da009e4cc56c0edf2c78ca4b799af6c70f98ee
pujanjoci <pujanjoci2@gmail.com>
2024-07-05 19:50:49 +0545

    Ch-2 and 3 added

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,8 @@ import { BiSearch } from 'react-icons/bi';
 
 import Box from './Box';
 import SidebarItem from './SidebarItem';
+import Library from './Library';
+
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -51,9 +53,15 @@ const Sidebar: React.FC<SidebarProps> = ({
                     </div>
                 </Box>
                 <Box className='overflow-y-auto h-full'>
-                    Song Library
+                    <Library>
+
+                    </Library>
                 </Box>
             </div>
+            <main className='h-full flex-1 overflow-y-auto py-2'>
+                {children}
+            </main>
+
         </div>
     );
 }
